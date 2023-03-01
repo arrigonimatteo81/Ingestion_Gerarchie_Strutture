1385abba65f8f450b35c06904e270526a7b453ed
matteo <matteo.arrigoni81@gmail.com>
2023-03-01 21:26:29 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import sys
-from os import path
 from types import SimpleNamespace
+
 
 from Classes.Ingestion.BuilderDefault import BuilderDefault
 from Classes.Ingestion.BuilderRedent import BuilderRedent
@@ -9,11 +9,11 @@ from Classes.Ingestion.BuilderRedpvi import BuilderRedpvi
 from Classes.Ingestion.BuilderRedstr import BuilderRedstr
 from Classes.Ingestion.BuilderRedven import BuilderRedven
 from Classes.ProcessLog.ProcessLog import ProcessLog
+from Etl.EtlRequestStrutture import EtlRequestStrutture
+from Process.SubProcessLogStrutture import SubProcessLogStrutture
+from Registro.RegistroStrutture import RegistroStrutture
 from Utils.constants import TAB_REDENT, TAB_REDVEN, TAB_REDSTR, TAB_REDPVI
 from Utils.utils import configure_log
-
-sys.path.append(path.abspath("../Classes"))
-from Classes.Etl.EtlRequestStrutture import EtlRequestStrutture
 
 
 def switch_classes(request: EtlRequestStrutture) -> BuilderDefault:
@@ -39,12 +39,21 @@ if __name__ == '__main__':
     #req = getEtlRequestBasedOnArguments(arguments)
     jsonEtlRequest = sys.argv[1]
     req = json.loads(jsonEtlRequest, object_hook=lambda d: SimpleNamespace(**d))
+    subprocessLog = SubProcessLogStrutture()
 
-    processLog = ProcessLog()
-    processLog.startProcessLog(req)
-    configure_log()
+    if not subprocessLog.SubprocessIsRunning(req):
+        subprocessLog.InitSubprocessLog(req)
 
-    cls = switch_classes(req)
+        configure_log()
 
-    etlResponse = cls.ingest()
-    processLog.endProcessLog(req, etlResponse)
+        cls = switch_classes(req)
+
+        etlResponse = cls.ingest()
+
+        if etlResponse.status == 'OK':
+            subprocessLog.CloseSubprocessLogOK(etlResponse)
+            registro = RegistroStrutture
+            registro.upsertRegistro(banca=req.semaforo.abi, tabella=req.semaforo.tabella,
+                                    semaforo_id=req.semaforo.id)
+        else:
+            subprocessLog.CloseSubprocessLogKO(etlResponse)
